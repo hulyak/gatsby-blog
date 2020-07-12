f76c57bbcadcbb2287320d3ad355191c1763e8a5
hulyak <57311842+hulya95@users.noreply.github.com>
2020-07-12 10:30:53 -0700

    feat : create index page and home page query

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,48 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 //components
 import Layout from 'components/Layout';
 import SEO from 'components/SEO';
+import Hero from 'components/Hero';
+import BlogPostCard from 'components/BlogPostCard';
 
-const IndexPage = () => <Layout>Start page</Layout>;
+// layout wrapper
+const IndexPage = ({ data }) => {
+  console.log(data);
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Hero />
+      <main>
+        <BlogPostCard />
+      </main>
+    </Layout>
+  );
+};
 
 export default IndexPage;
+
+// http://localhost:8000/___graphql query blogs
+export const indexQuery = graphql`
+  query blogListQuery {
+    allMarkdownRemark(
+      filter: { frontmatter: { type: { eq: "post" } } }
+      sort: { fields: frontmatter___date, order: DESC }
+    ) {
+      edges {
+        node {
+          fields {
+            readingTime {
+              text
+            }
+          }
+          frontmatter {
+            date
+            title
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,19 +1,17 @@
-import {
-  createGlobalStyle
-} from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
 
-import MuliRegular from 'fonts/Muli-Regular.woff'
+import MuliRegular from 'fonts/Muli-Regular.woff';
 import MuliBlack from 'fonts/Muli-Black.woff';
 
 // create js object with color
 export const colors = {
-  white: "#fff",
-  darkGrey: "#1a1c20",
-  deepDarkGrey: "#404040",
-  blueShade1: "#215973",
-  blueShade2: "#6fadc6",
-  blueShade3: "#d1e1e9"
-}
+  white: '#fff',
+  darkGrey: '#1a1c20',
+  deepDarkGrey: '#404040',
+  blueShade1: '#215973',
+  blueShade2: '#6fadc6',
+  blueShade3: '#d1e1e9',
+};
 
 // dark theme object
 export const darkTheme = {
@@ -22,9 +20,9 @@ export const darkTheme = {
     menuBackground: colors.blueShade1,
     textDark: colors.blueShade3,
     textMain: colors.blueShade2,
-    textSecondary: colors.blueShade3
-  }
-}
+    textSecondary: colors.blueShade3,
+  },
+};
 
 export const lightTheme = {
   colors: {
@@ -32,12 +30,11 @@ export const lightTheme = {
     menuBackground: colors.blueShade3,
     textDark: colors.blueShade1,
     textMain: colors.blueShade2,
-    textSecondary: colors.deepDarkGrey
+    textSecondary: colors.deepDarkGrey,
   },
-}
+};
 
-
-export const GlobalStyles = createGlobalStyle `
+export const GlobalStyles = createGlobalStyle`
   :root {
     --site-max-width: 1200px;
   }
@@ -59,10 +56,10 @@ export const GlobalStyles = createGlobalStyle `
      font-style: normal;
    }
 
-${'' /* theme prop is available to all style components */}
+${'' /* theme prop is available to all styled components */}
    body {
-     background : ${props => props.theme.colors.background};
-     color : ${props => props.theme.colors.textSecondary};
+     background : ${(props) => props.theme.colors.background};
+     color : ${(props) => props.theme.colors.textSecondary};
      font-family: 'Muli Regular', Arial, Helvetica, sans - serif;
      margin :0;
      padding : 0 20px 80px 20px;
@@ -74,7 +71,7 @@ ${'' /* theme prop is available to all style components */}
      margin : 40px auto;
     }
     h1, h2, h3, h4, h5, h6 {
-      color : ${props => props.theme.colors.textMain};
+      color : ${(props) => props.theme.colors.textMain};
       font-family :'Muli Black', Arial, Helvetica, sans-serif;
       transition : all 0.5s ease;
       letter-spacing : 1px;
@@ -113,4 +110,4 @@ ${'' /* theme prop is available to all style components */}
      a{ 
        text-decoration : none;
      }
-`
+`;
