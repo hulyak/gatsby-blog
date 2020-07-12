9e2c672cba6eb34a2cd19b799bfa2466c322a747
hulyak <57311842+hulya95@users.noreply.github.com>
2020-07-12 12:15:56 -0700

    feat: create the background and styles

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,5 +1,33 @@
 import React from 'react';
+import { Link } from 'gatsby';
+//component
+import { useHeroQuery } from 'hooks/useHeroQuery';
+import BGImage from 'components/BGImage';
+//hooks
+//styles
+import { HeroWrapper, TextWrapper } from './Hero.styles';
 
-const Hero = () => <div>Hero</div>;
+const Hero = () => {
+  // destructure
+  const {
+    heroImage,
+    heroImageText,
+    heroImageBtnText,
+    heroImageBtnLink,
+  } = useHeroQuery();
+
+  return (
+    <HeroWrapper>
+      <BGImage title="heroImage" fluid={heroImage.childImageSharp.fluid}>
+        <TextWrapper>
+          <h2>{heroImageText}</h2>
+          <Link to={heroImageBtnLink}>
+            <button>{heroImageBtnText}</button>
+          </Link>
+        </TextWrapper>
+      </BGImage>
+    </HeroWrapper>
+  );
+};
 
 export default Hero;
