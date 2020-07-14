d52f24c9148144198c60010af19f9843b8f51a8b
hulyak <57311842+hulya95@users.noreply.github.com>
2020-07-13 17:06:51 -0700

    create mode context for dark-light mode

---
+++ src/context/ModeProvider.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+// dark-light mode
+//wraprootelement
+//global state hooks and context
+export const ModeContext = React.createContext([]);
+
+const ModeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(true);
+  return (
+    <ModeContext.Provider value={[darkMode, setDarkMode]}>
+      {children}
+    </ModeContext.Provider>
+  );
+};
+
+export default ModeProvider;
