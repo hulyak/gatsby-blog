5d9360bd1e3216de43ef189a7bb699040df98b57
hulyak <57311842+hulya95@users.noreply.github.com>
2020-07-13 17:08:18 -0700

    feat: create wrapRootElement and export

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,9 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
+import React from 'react';
+import ModeProvider from 'context/ModeProvider';
 
-// You can delete this file if you're not using it
+export const wrapRootElement = ({ element }) => (
+  <ModeProvider>{element}</ModeProvider>
+);
