c7a4acaa484d23a040a867050e16ff0167ab4ba8
hulyak <57311842+hulya95@users.noreply.github.com>
2020-07-13 17:13:36 -0700

    feat:implement the button in layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ThemeProvider } from 'styled-components';
 //hooks
 import { useMetaDataQuery } from 'hooks/useMetaDataQuery';
 //components
 import Header from 'components/Header';
+//Context
+import { ModeContext } from 'context/ModeProvider';
 //styles
 import { GlobalStyles, lightTheme, darkTheme } from 'styles/GlobalStyles';
 
 // react component children
 const Layout = ({ children }) => {
   const data = useMetaDataQuery();
+  const [darkMode] = useContext(ModeContext);
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <GlobalStyles />
       <Header siteTitle={data.title} />
       {children}
