250e79217eccb4424e28b795505c39d605c717be
hulyak <57311842+hulya95@users.noreply.github.com>
2020-07-14 07:55:58 -0700

    feat: create the slug for pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,62 @@
 const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
 
+//First create the slug for posts and pages
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: `content`,
+    });
+
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    });
+  }
+};
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const content = await graphql(`
+  {
+    posts : allMarkdownRemark(
+      filter : {frontmatter : {type : {eq : "post"}}} 
+    ){
+      edges {
+        node {
+          frontmatter {
+            published
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+    pages : allMarkdownRemark (
+      filter : {frontmatter : { type : {eq : "page"}}}
+    ){
+      edges{
+        node {
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+  `);
+  console.log(content);
+};
 //For absolute imports
-exports.onCreateWebpackConfig = ({
-  actions
-}) => {
+exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
       modules: [path.resolve(__dirname, 'src'), 'node_modules'],
     },
-  })
-}
+  });
+};
